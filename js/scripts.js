7a88c0d8dc59a341c7f7fde161c648b05374f01d
bananyog <bananyog@gmail.com>
2018-04-14 12:39:32 -0700

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
+
+
+//alert(2 + 2);
+//alert(4 * "2");
+//alert(8 + "2");
